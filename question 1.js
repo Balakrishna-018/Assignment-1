3a7c549e7adf66df08c0900dcae81a72d6c86bfc
Balakrishna-018 <82585124+Balakrishna-018@users.noreply.github.com>
2021-04-15 16:55:43 +0530

    Add files via upload

---
+++ question 1.js
@@ -0,0 +1,3 @@
+const michael = "People say I'm the best boss."
+console.log(michael.length+" "+michael.slice(15)+michael.slice(11,29),michael.slice(11,-1))
+console.log(michael.slice(-5), michael.slice(-10,-6))
---
+++ question 3.js
@@ -0,0 +1,16 @@
+let starHollows = ['Lorelai', 'Rory', 'Luke', 'Lane', 'Kirk', 'Taylor'];
+let removed = starHollows.splice(1);
+console.log(removed)
+let beverlyHillsGang = ['Brandon', 'Brenda', 'Dylan', 'Andrea', 'Steve', 'Kelly', 'Donna',
+'David'];
+const remove = beverlyHillsGang.splice(1, 2);
+console.log(remove)
+let seinfeld = ['Jerry', 'George', 'Kramer', 'Newman', 'Elaine'];
+let detach = seinfeld.splice(-2, 1); 
+console.log(detach)
+let brooklynNineNine = ['Jake', 'Amy', 'Charles','Rosa', 'Captain Holt', 'Gina', 'Terry'];
+let removedAndInserted = (brooklynNineNine.splice(5,1),'Hitchcock', 'Scully');
+console.log(removedAndInserted)
+let friends = ['Ross', 'Rachel', 'Joey', 'Chandler', 'Monica', 'Phoebe'];
+let insertedOnly = (friends.splice(1,1),"Gunther", 'Carol', 'Susan')
+console.log(insertedOnly)
